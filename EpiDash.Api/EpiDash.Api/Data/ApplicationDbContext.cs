632345258717454a6e2af5ca23d8a14aa3add63d
Vinesh Raghvani <vineshsh@ualberta.ca>
2026-04-16 15:39:47 -0600

    Scaffolded .NET 10 Web API and configured EF Core database context

---
+++ EpiDash.Api/EpiDash.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using EpiDash.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EpiDash.Api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // This tells EF Core to create a table called "AirQualityRecords"
+        public DbSet<AirQualityRecord> AirQualityRecords { get; set; }
+    }
+}
---
+++ EpiDash.Api/EpiDash.Api/Models/AirQualityRecord.cs
@@ -0,0 +1,11 @@
+﻿namespace EpiDash.Api.Models
+{
+    public class AirQualityRecord
+    { 
+        public int Id { get; set; } // Primary Key
+        public required string LocationName { get; set; }
+        public double AirQualityIndex { get; set; }
+        public double Pm25Level { get; set; } // Particulate matter (e.g., wildfire smoke)
+        public DateTime RecordDate { get; set; } = DateTime.UtcNow;
+    }
+}
